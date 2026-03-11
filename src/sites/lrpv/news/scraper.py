9c6ea17d5d6d4be1a40807d8bc426d8242e05278
Alexander Popov <alexander.popov.dev@gmail.com>
2026-03-11 12:20:08 +0200

    Add a user agent

--- src/sites/lrpv/news/scraper.py
+++ src/sites/lrpv/news/scraper.py
@@ -6,7 +6,14 @@ from src.core.dto import RequestDTO, ResponseDTO
 class NewsScraper(BaseScraper):
     @retry(retries=3, delay=10)
     def run(self, url: str) -> ResponseDTO:
-        request_dto = RequestDTO(method="GET", url=url, timeout=30)
+        headers = {
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/144.0.0.0 Safari/537.36"
+            )
+        }
+        request_dto = RequestDTO(method="GET", url=url, headers=headers, timeout=30)
         response_dto = self._client.fetch(request_dto=request_dto)
 
         return response_dto
